28e0add0b6f68940742146a2948e646f6e930e02
Nalinish Ranjan <nalinishranjan05@gmail.com>
2026-04-19 11:07:43 +0530

    Revert "techpack: audio: asoc: Report correct key code of headset buttons"
    This reverts commit 220a04ec70faadaa871a8303e8447a423f18f3e1.
    
    * needed by mido

--- asoc/msm8952.c
+++ asoc/msm8952.c
@@ -82,9 +82,15 @@ static struct wcd_mbhc_config mbhc_cfg = {
 	.swap_gnd_mic = NULL,
 	.hs_ext_micbias = false,
 	.key_code[0] = KEY_MEDIA,
-	.key_code[1] = KEY_VOLUMEUP,
-	.key_code[2] = KEY_VOLUMEDOWN,
+#ifdef CONFIG_MACH_XIAOMI_MIDO
+	.key_code[1] = BTN_1,
+	.key_code[2] = BTN_2,
 	.key_code[3] = 0,
+#else
+	.key_code[1] = KEY_VOICECOMMAND,
+	.key_code[2] = KEY_VOLUMEUP,
+	.key_code[3] = KEY_VOLUMEDOWN,
+#endif
 	.key_code[4] = 0,
 	.key_code[5] = 0,
 	.key_code[6] = 0,
